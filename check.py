456e607f0b50707be0c9ebb5ecf7684cc2704c0d
aadhi95 <aadhi95balaji@gmail.com>
2016-06-03 14:51:16 +0530

    Update check.py

--- check.py
+++ check.py
@@ -1,6 +1,9 @@
+import os
 separator = "="
 key = []
 key1 = []
+os.system('wget https://github.com/aadhi95/configcheck/raw/master/service.properties')
+os.system('wget https://github.com/aadhi95/configcheck/raw/master/reference.properties')
 with open('service.properties') as f:
 	for line in f:
 		if separator in line:
@@ -11,7 +14,10 @@ with open('reference.properties') as f:
 		if separator in line:
 			name, value = line.split(separator, 1)
 			key1.append(name)
-if len(set(key).intersection(key1))==len(key):
-	print(1)
+
+if len(set(key).intersection(set(key1)))==len(set(key)):
+	print("matches")
 else:
-	print(0)
+	print("dosent match")
+os.system('rm service.properties');
+os.system('rm reference.properties')
