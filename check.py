29cd3ac6c948d380ce6b46ede8e96edb9ad47c40
aadhi95 <aadhi95balaji@gmail.com>
2016-06-03 14:16:20 +0530

    Create check.py

---
+++ check.py
@@ -0,0 +1,17 @@
+separator = "="
+key = []
+key1 = []
+with open('service.properties') as f:
+	for line in f:
+		if separator in line:
+			name, value = line.split(separator, 1)
+			key.append(name)
+with open('reference.properties') as f:
+	for line in f:
+		if separator in line:
+			name, value = line.split(separator, 1)
+			key1.append(name)
+if len(set(key).intersection(key1))==len(key):
+	print("matches")
+else:
+	print("dosent match")
